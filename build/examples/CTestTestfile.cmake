c400d78eab67e66c834aadc3f2b4c16397346ccd
Radu Burcea <rfb@datarespons.no>
2022-01-14 09:33:56 +0100

    delete build dir and update .gitignore

--- build/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radu/Documents/AutoStore/github/WorkdayCalender-Library/examples
-# Build directory: /home/radu/Documents/AutoStore/github/WorkdayCalender-Library/build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
